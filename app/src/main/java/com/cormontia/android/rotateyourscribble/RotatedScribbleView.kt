7a8f789c31ae1606f75dacc8468cb59727e45a8d
FractionalRadix <nerostjust@gmail.com>
2022-05-09 21:46:16 +0200

    Run animations in response to fling gestures.

--- app/src/main/java/com/cormontia/android/rotateyourscribble/RotatedScribbleView.kt
+++ app/src/main/java/com/cormontia/android/rotateyourscribble/RotatedScribbleView.kt
@@ -8,10 +8,14 @@ import android.graphics.Paint
 import android.graphics.PointF
 import android.util.AttributeSet
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
-import android.view.animation.LinearInterpolator
+import android.view.animation.DecelerateInterpolator
+import androidx.core.view.GestureDetectorCompat
 import com.cormontia.android.rotateyourscribble.matrix3d.MatrixFactory
 import com.cormontia.android.rotateyourscribble.matrix3d.Vec4
+import kotlin.math.abs
 
 /**
  * Show the 3D wireframe that you get, when you rotate the scribble around the Y-axis.
@@ -30,10 +34,14 @@ class RotatedScribbleView : View {
         blackPaint.style = Paint.Style.FILL_AND_STROKE
     }
 
-    //private val anim = ValueAnimator() // TODO?~ Use ".ofFloat(...)" or ".ofInt(...)" ?
-    private val anim = ValueAnimator.ofFloat(0.0f, (2.0f * Math.PI).toFloat())
-    private var zRotation = 0.0f
+    private lateinit var mGestureDetector: GestureDetectorCompat
 
+    private val rightwardAnimator = ValueAnimator.ofFloat(0.0f, (2.0f * Math.PI).toFloat())
+    private val leftwardAnimator = ValueAnimator.ofFloat((2.0f * Math.PI).toFloat(), 0.0f)
+    private var yRotation = 0.0f
+    private val upwardAnimator = ValueAnimator.ofFloat(0.0f, (2.0f * Math.PI).toFloat())
+    private val downwardAnimator = ValueAnimator.ofFloat((2.0f * Math.PI).toFloat(), 0.0f)
+    private var xRotation = 0.0f
 
     constructor(context: Context) : super(context) {
         init(null, 0)
@@ -59,15 +67,40 @@ class RotatedScribbleView : View {
 
         a.recycle()
 
-        anim.addUpdateListener {
-            zRotation = it.animatedValue as Float //TODO?~ Use it.animatedFraction instead?
+        mGestureDetector = GestureDetectorCompat(context, FlingGestureListener())
+
+        leftwardAnimator.addUpdateListener {
+            yRotation = it.animatedValue as Float //TODO?~ Use it.animatedFraction instead?
             rotatedLines = rotate(basePoints)
             invalidate()
-            Log.i("RotatedScribbleView", "animation at $zRotation")
         }
-        //anim.interpolator = DecelerateInterpolator()
-        anim.interpolator = LinearInterpolator()
-        anim.duration = 2000 // Should be in microseconds...
+        leftwardAnimator.interpolator = DecelerateInterpolator()
+        leftwardAnimator.duration = 2000
+
+        rightwardAnimator.addUpdateListener {
+            yRotation = it.animatedValue as Float //TODO?~ Use it.animatedFraction instead?
+            rotatedLines = rotate(basePoints)
+            invalidate()
+        }
+        rightwardAnimator.interpolator = DecelerateInterpolator()
+        rightwardAnimator.duration = 2000
+
+        upwardAnimator.addUpdateListener {
+            xRotation = it.animatedValue as Float //TODO?~ Use it.animatedFraction instead?
+            rotatedLines = rotate(basePoints)
+            invalidate()
+        }
+        upwardAnimator.interpolator = DecelerateInterpolator()
+        upwardAnimator.duration = 2000
+
+        downwardAnimator.addUpdateListener {
+            xRotation = it.animatedValue as Float //TODO?~ Use it.animatedFraction instead?
+            rotatedLines = rotate(basePoints)
+            invalidate()
+        }
+        downwardAnimator.interpolator = DecelerateInterpolator()
+        downwardAnimator.duration = 2000
+
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -94,7 +127,7 @@ class RotatedScribbleView : View {
         basePoints = points
         rotatedLines = rotate(basePoints)
         invalidate()
-        anim.start()
+        //anim.start()
     }
 
     fun setCenter(centerX: Double, centerY: Double) {
@@ -110,9 +143,11 @@ class RotatedScribbleView : View {
         for (angleInDegrees in 0 until 360 step 5) {
             val angleInRadians = Math.toRadians(angleInDegrees.toDouble())
             val rotation = MatrixFactory.RotateAroundY(angleInRadians)
-            //val fullMatrix = translateAfter * rotation * translateBefore
-            val animationRotation = MatrixFactory.RotateAroundX(zRotation.toDouble())
-            val fullMatrix = translateAfter * animationRotation * rotation * translateBefore
+
+            val animationRotationX = MatrixFactory.RotateAroundX(xRotation.toDouble())
+            val animationRotationY = MatrixFactory.RotateAroundY(yRotation.toDouble())
+
+            val fullMatrix = translateAfter * animationRotationX * animationRotationY * rotation * translateBefore
 
             val rotatedLine = mutableListOf<PointF>()
             for (point in points) {
@@ -125,5 +160,58 @@ class RotatedScribbleView : View {
             rotatedLines.add(rotatedLine)
         }
         return rotatedLines
+    }
+
+    // Gesture detection
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        Log.i("RotatedScribbleView", "Entered onTouchEvent")
+        mGestureDetector.onTouchEvent(event)
+        if (event != null) {
+            if (event.actionMasked == MotionEvent.ACTION_DOWN) {
+                return true
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+    inner class FlingGestureListener: GestureDetector.SimpleOnGestureListener() {
+        // Should almost always be overridden to return `true`.
+        // "If you return false from onDown(), as GestureDetector.SimpleOnGestureListener does by default,
+        //  the system assumes that you want to ignore the rest of the gesture, and the other methods of GestureDetector.OnGestureListener never get called."
+        // (Source: https://developer.android.com/training/gestures/detector#detect-a-subset-of-supported-gestures )
+        override fun onDown(e: MotionEvent): Boolean {
+            Log.i("RotatedScribbleView.FlingGestureListener", "Entered onDown(...)")
+            return true
+        }
+
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            Log.i("RotatedScribbleView.FlingGestureListener", "Entered onFling(...)")
+            if (e1 != null && e2 != null) {
+                val rightward = e2.x - e1.x
+                val upward = e2.y - e1.y
+                if (abs(rightward) > abs(upward)) {
+                    if (rightward > 0) {
+                        Log.i("RotatedScribbleView", "User flinged to rotate rightward.")
+                        rightwardAnimator.start()
+                    } else {
+                        Log.i("RotatedScribbleView", "User flinged to rotate leftward.")
+                        leftwardAnimator.start()
+                    }
+                } else {
+                    if (upward > 0) {
+                        upwardAnimator.start()
+                    } else {
+                        downwardAnimator.start()
+                    }
+                }
+            }
+            return super.onFling(e1, e2, velocityX, velocityY)
+        }
     }
 }
