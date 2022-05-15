cfcd6bab0caae696457a013ea9e0f9ffc7dc8cf2
FractionalRadix <nerostjust@gmail.com>
2022-05-15 12:55:42 +0200

    Implement Load and Save functionality (untested).

--- app/src/main/java/com/cormontia/android/rotateyourscribble/MainActivity.kt
+++ app/src/main/java/com/cormontia/android/rotateyourscribble/MainActivity.kt
@@ -1,24 +1,27 @@
 package com.cormontia.android.rotateyourscribble
 
+import android.content.Intent
 import android.graphics.PointF
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ImageButton
 import androidx.activity.viewModels
+import java.io.*
+import kotlin.streams.toList
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: ScribbleViewModel by viewModels()
 
-    private lateinit var flatScribbleView : FlatScribbleView
+    private val storageFileMimeType = "text/plain"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rotatedScribbleView = findViewById<RotatedScribbleView>(R.id.rotatedScribbleview)
-        flatScribbleView = findViewById<FlatScribbleView>(R.id.flatScribbleView)
+        val flatScribbleView = findViewById<FlatScribbleView>(R.id.flatScribbleView)
 
         viewModel.points.observe(this) {
             list -> run {
@@ -49,19 +52,81 @@ class MainActivity : AppCompatActivity() {
         viewModel.clear()
     }
 
+    private val loadCode = 14
     private fun load() {
-        //TODO()
+        val loadIntent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        loadIntent.addCategory(Intent.CATEGORY_OPENABLE)
+        loadIntent.type = storageFileMimeType
+        startActivityForResult(loadIntent, loadCode) //TODO!~ Use "registerForActivityResult" instead.
     }
 
+    private val saveCode = 21
     private fun save() {
-        //TODO()
+        // "Note: ACTION_CREATE_DOCUMENT cannot overwrite an existing file.
+        //  If your app tries to save a file with the same name, the system appends a number in parentheses at the end of the file name."
+        // Source: https://developer.android.com/training/data-storage/shared/documents-files#create-file
+        val saveIntent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+        saveIntent.type = storageFileMimeType
+        saveIntent.putExtra(Intent.EXTRA_TITLE, "scribble.txt") //TODO?~ Add timestamp or something to make it unique?
+        startActivityForResult(saveIntent, saveCode) //TODO!~ Use "registerForActivityResult" instead.
     }
 
+    private val exportCode = 28
     private fun export() {
-        //TODO()
+        // "Note: ACTION_CREATE_DOCUMENT cannot overwrite an existing file.
+        //  If your app tries to save a file with the same name, the system appends a number in parentheses at the end of the file name."
+        // Source: https://developer.android.com/training/data-storage/shared/documents-files#create-file
+        val exportIntent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+        exportIntent.type = storageFileMimeType
+        exportIntent.putExtra(Intent.EXTRA_TITLE, "scribble.obj") //TODO?~ Add timestamp or something to make it unique?
+        startActivityForResult(exportIntent, exportCode) //TODO!~ Use "registerForActivityResult" instead.
     }
 
     private fun share() {
         //TODO()
+    }
+
+    @Deprecated("'onActivityResult(Int, Int, Intent?): Unit' is deprecated. Overrides deprecated member in 'androidx.activity.ComponentActivity'. Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        super.onActivityResult(requestCode, resultCode, resultData)
+
+        val uri = resultData?.data
+        when (requestCode) {
+            loadCode -> {
+                if (uri != null) {
+                    //TODO!+ Handle FileNotFoundException and IOException
+                    val contentResolver = applicationContext.contentResolver
+                    val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "r")
+                    val fileReader = FileReader(parcelFileDescriptor?.fileDescriptor)
+                    val stringList = BufferedReader(fileReader)
+                        .lines()
+                        .filter { !it.isNullOrBlank() }
+                        .toList()
+                    val points = viewModel.deserializePointsList(stringList)
+                    viewModel.accept(points as MutableList<PointF>)
+                }
+            }
+            saveCode -> {
+                if (uri != null) {
+                    //TODO!+ Handle FileNotFoundException and IOException
+                    val contentResolver = applicationContext.contentResolver
+                    val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "w")
+                    val fileWriter = FileWriter(parcelFileDescriptor?.fileDescriptor)
+                    val bufferedWriter = BufferedWriter(fileWriter)
+
+                    bufferedWriter.use {
+                        val pointsAsStrings = viewModel.serializePointsList()
+                        pointsAsStrings.forEach { line ->
+                            bufferedWriter.appendLine(line)
+                        }
+                    }
+                }
+            }
+            exportCode -> {
+                if (uri != null) {
+                    //TODO!+ Export to WaveFront.OBJ format
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/cormontia/android/rotateyourscribble/ScribbleViewModel.kt
+++ app/src/main/java/com/cormontia/android/rotateyourscribble/ScribbleViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 
 class ScribbleViewModel : ViewModel() {
 
+    //TODO?- Is the backing field still needed?
     private val points_backingField = mutableListOf<PointF>()
     //TODO?~ Use LiveData instead of MutableLiveData, so that external parties can't update this field?
     var points: MutableLiveData<MutableList<PointF>> = MutableLiveData()
@@ -14,11 +15,12 @@ class ScribbleViewModel : ViewModel() {
     }
 
     var centerX = 0.0 // Placeholder value, since primitive types cannot use "lateinit".
-        private set;
+        private set
 
     var centerY = 0.0 // Placeholder value, since primitive types cannot use "lateinit".
-        private set;
+        private set
 
+    //TODO?~ use List<PointF> as parameter, instead of MutableList<PointF> ?
     fun accept(newPoints: MutableList<PointF>) {
         points_backingField.addAll(newPoints)
         points.value = points_backingField
@@ -32,5 +34,29 @@ class ScribbleViewModel : ViewModel() {
     fun clear() {
         points_backingField.clear()
         points.value = points_backingField
+    }
+
+    //TODO?~ Make this thread-safe? Is it possible for the points to be changed DURING serialization?
+    fun serializePointsList(): List<String> {
+        val serialization = mutableListOf<String>()
+        for (point in points_backingField) {
+            serialization.add("${point.x} ${point.y}")
+        }
+        return serialization
+    }
+
+    fun deserializePointsList(stringList: List<String>): List<PointF> {
+        val points = mutableListOf<PointF>()
+        for (point in stringList) {
+            val split = point.split(' ')
+            if (split.size != 2) {
+                //TODO!+ Issue an error
+            } else {
+                val x = split[0].toFloat()
+                val y = split[1].toFloat()
+                points.add(PointF(x,y))
+            }
+        }
+        return points
     }
 }
