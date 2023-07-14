856db390b5faadeba44888c6ad1874870315ad47
FractionalRadix <nerostjust@gmail.com>
2023-07-14 21:39:22 +0200

    Use `registerForActivityResult` instead of `startActivityForResult`.

--- app/src/main/java/com/cormontia/android/rotateyourscribble/MainActivity.kt
+++ app/src/main/java/com/cormontia/android/rotateyourscribble/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.cormontia.android.rotateyourscribble
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.PointF
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.viewModels
 import java.io.*
 import kotlin.streams.toList
@@ -14,6 +20,74 @@ class MainActivity : AppCompatActivity() {
     private val viewModel: ScribbleViewModel by viewModels()
 
     private val storageFileMimeType = "text/plain"
+
+    private val loadLauncher = registerForActivityResult(
+        //TODO?~ See if we can have a launcher that does NOT require input (0 parameters instead of a dummy String parameter).
+        object: ActivityResultContract<String, Uri>() {
+            override fun createIntent(context: Context, input: String?): Intent {
+                val loadIntent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+                loadIntent.addCategory(Intent.CATEGORY_OPENABLE)
+                loadIntent.type = storageFileMimeType
+                return loadIntent
+            }
+
+            override fun parseResult(resultCode: Int, intent: Intent?): Uri {
+                return intent?.data!! //TODO!+ Some NULL handling...
+            }
+        },
+        object: ActivityResultCallback<Uri> {
+            override fun onActivityResult(result: Uri?) {
+                load(result)
+            }
+        }
+    )
+
+    private val saveLauncher = registerForActivityResult(
+        //TODO?~ See if we can have a launcher that does NOT require input (0 parameters instead of a dummy String parameter).
+        object: ActivityResultContract<String, Uri>() {
+            override fun createIntent(context: Context, input: String?): Intent {
+                // "Note: ACTION_CREATE_DOCUMENT cannot overwrite an existing file.
+                //  If your app tries to save a file with the same name, the system appends a number in parentheses at the end of the file name."
+                // Source: https://developer.android.com/training/data-storage/shared/documents-files#create-file
+                val saveIntent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+                saveIntent.type = storageFileMimeType
+                saveIntent.putExtra(Intent.EXTRA_TITLE, "scribble.txt") //TODO?~ Add timestamp or something to make it unique?
+                return saveIntent
+            }
+
+            override fun parseResult(resultCode: Int, intent: Intent?): Uri {
+                return intent?.data!! //TODO!+ Some NULL handling...
+            }
+        },
+        object: ActivityResultCallback<Uri> {
+            override fun onActivityResult(result: Uri?) {
+                save(result)
+            }
+        }
+    )
+
+    private val exportLauncher = registerForActivityResult(
+        object: ActivityResultContract<String, Uri>() {
+            override fun createIntent(context: Context, input: String?): Intent {
+                // "Note: ACTION_CREATE_DOCUMENT cannot overwrite an existing file.
+                //  If your app tries to save a file with the same name, the system appends a number in parentheses at the end of the file name."
+                // Source: https://developer.android.com/training/data-storage/shared/documents-files#create-file
+                val exportIntent = Intent(Intent.ACTION_CREATE_DOCUMENT)
+                exportIntent.type = storageFileMimeType
+                exportIntent.putExtra(Intent.EXTRA_TITLE, "scribble.obj") //TODO?~ Add timestamp or something to make it unique?
+                return exportIntent
+            }
+
+            override fun parseResult(resultCode: Int, intent: Intent?): Uri {
+                return intent?.data!! //TODO!+ Some NULL handling...
+            }
+        },
+        object: ActivityResultCallback<Uri> {
+            override fun onActivityResult(result: Uri?) {
+                export(result)
+            }
+        }
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,9 +110,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<ImageButton>(R.id.clearButton).setOnClickListener{ clear() }
-        findViewById<ImageButton>(R.id.loadButton).setOnClickListener{ load() }
-        findViewById<ImageButton>(R.id.saveButton).setOnClickListener{ save() }
-        findViewById<ImageButton>(R.id.exportButton).setOnClickListener{ export() }
+        findViewById<ImageButton>(R.id.loadButton).setOnClickListener{ loadLauncher.launch("Dummy") }
+        findViewById<ImageButton>(R.id.saveButton).setOnClickListener{ saveLauncher.launch("Dummy") }
+        findViewById<ImageButton>(R.id.exportButton).setOnClickListener{ exportLauncher.launch("Dummy") }
         findViewById<ImageButton>(R.id.shareButton).setOnClickListener{ share() }
     }
 
@@ -54,36 +128,6 @@ class MainActivity : AppCompatActivity() {
         viewModel.clear()
     }
 
-    private val loadCode = 14
-    private fun load() {
-        val loadIntent = Intent(Intent.ACTION_OPEN_DOCUMENT)
-        loadIntent.addCategory(Intent.CATEGORY_OPENABLE)
-        loadIntent.type = storageFileMimeType
-        startActivityForResult(loadIntent, loadCode) //TODO!~ Use "registerForActivityResult" instead.
-    }
-
-    private val saveCode = 21
-    private fun save() {
-        // "Note: ACTION_CREATE_DOCUMENT cannot overwrite an existing file.
-        //  If your app tries to save a file with the same name, the system appends a number in parentheses at the end of the file name."
-        // Source: https://developer.android.com/training/data-storage/shared/documents-files#create-file
-        val saveIntent = Intent(Intent.ACTION_CREATE_DOCUMENT)
-        saveIntent.type = storageFileMimeType
-        saveIntent.putExtra(Intent.EXTRA_TITLE, "scribble.txt") //TODO?~ Add timestamp or something to make it unique?
-        startActivityForResult(saveIntent, saveCode) //TODO!~ Use "registerForActivityResult" instead.
-    }
-
-    private val exportCode = 28
-    private fun export() {
-        // "Note: ACTION_CREATE_DOCUMENT cannot overwrite an existing file.
-        //  If your app tries to save a file with the same name, the system appends a number in parentheses at the end of the file name."
-        // Source: https://developer.android.com/training/data-storage/shared/documents-files#create-file
-        val exportIntent = Intent(Intent.ACTION_CREATE_DOCUMENT)
-        exportIntent.type = storageFileMimeType
-        exportIntent.putExtra(Intent.EXTRA_TITLE, "scribble.obj") //TODO?~ Add timestamp or something to make it unique?
-        startActivityForResult(exportIntent, exportCode) //TODO!~ Use "registerForActivityResult" instead.
-    }
-
     private val shareCode = 35
     private fun share() {
         val sharingIntent = Intent(Intent.ACTION_SEND)
@@ -96,58 +140,52 @@ class MainActivity : AppCompatActivity() {
         startActivity(Intent.createChooser(sharingIntent, "Share using: "))
     }
 
-    @Deprecated("'onActivityResult(Int, Int, Intent?): Unit' is deprecated. Overrides deprecated member in 'androidx.activity.ComponentActivity'. Deprecated in Java")
-    override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
-        super.onActivityResult(requestCode, resultCode, resultData)
+    private fun load(uri: Uri?) {
+        if (uri != null) {
+            //TODO!+ Handle FileNotFoundException and IOException
+            val contentResolver = applicationContext.contentResolver
+            val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "r")
+            val fileReader = FileReader(parcelFileDescriptor?.fileDescriptor)
+            val stringList = BufferedReader(fileReader)
+                .lines()
+                .filter { !it.isNullOrBlank() }
+                .toList()
+            val points = viewModel.deserializePointsList(stringList)
+            viewModel.clear()
+            viewModel.accept(points as MutableList<PointF>)
+        }
+    }
 
-        val uri = resultData?.data
-        when (requestCode) {
-            loadCode -> {
-                if (uri != null) {
-                    //TODO!+ Handle FileNotFoundException and IOException
-                    val contentResolver = applicationContext.contentResolver
-                    val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "r")
-                    val fileReader = FileReader(parcelFileDescriptor?.fileDescriptor)
-                    val stringList = BufferedReader(fileReader)
-                        .lines()
-                        .filter { !it.isNullOrBlank() }
-                        .toList()
-                    val points = viewModel.deserializePointsList(stringList)
-                    viewModel.clear()
-                    viewModel.accept(points as MutableList<PointF>)
+    private fun save(uri: Uri?) {
+        if (uri != null) {
+            //TODO!+ Handle FileNotFoundException and IOException
+            val contentResolver = applicationContext.contentResolver
+            val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "w")
+            val fileWriter = FileWriter(parcelFileDescriptor?.fileDescriptor)
+            val bufferedWriter = BufferedWriter(fileWriter)
+
+            bufferedWriter.use {
+                val pointsAsStrings = viewModel.serializePointsList()
+                pointsAsStrings.forEach { line ->
+                    bufferedWriter.appendLine(line)
                 }
             }
-            saveCode -> {
-                if (uri != null) {
-                    //TODO!+ Handle FileNotFoundException and IOException
-                    val contentResolver = applicationContext.contentResolver
-                    val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "w")
-                    val fileWriter = FileWriter(parcelFileDescriptor?.fileDescriptor)
-                    val bufferedWriter = BufferedWriter(fileWriter)
+        }
+    }
 
-                    bufferedWriter.use {
-                        val pointsAsStrings = viewModel.serializePointsList()
-                        pointsAsStrings.forEach {
-                            line -> bufferedWriter.appendLine(line)
-                        }
-                    }
-                }
-            }
-            exportCode -> {
-                if (uri != null) {
-                    //TODO!+ Handle FileNotFoundException and IOException
-                    val contentResolver = applicationContext.contentResolver
-                    val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "w")
-                    val fileWriter = FileWriter(parcelFileDescriptor?.fileDescriptor)
-                    val bufferedWriter = BufferedWriter(fileWriter)
+    private fun export(uri: Uri?) {
+        if (uri != null) {
+            //TODO!+ Handle FileNotFoundException and IOException
+            val contentResolver = applicationContext.contentResolver
+            val parcelFileDescriptor = contentResolver.openFileDescriptor(uri, "w")
+            val fileWriter = FileWriter(parcelFileDescriptor?.fileDescriptor)
+            val bufferedWriter = BufferedWriter(fileWriter)
 
-                    val textLines = viewModel.toWavefrontFormat()
-                    bufferedWriter.use {
-                        //TODO!~ Currently stores as .obj.txt ...
-                        textLines.forEach {
-                            line -> bufferedWriter.appendLine(line)
-                        }
-                    }
+            val textLines = viewModel.toWavefrontFormat()
+            bufferedWriter.use {
+                //TODO!~ Currently stores as .obj.txt ...
+                textLines.forEach { line ->
+                    bufferedWriter.appendLine(line)
                 }
             }
         }
